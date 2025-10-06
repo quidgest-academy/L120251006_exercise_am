caf073fc693cae47b9f2f5fc483454055c5dbd3f
abdulhak.moussa <abdulhak.moussa@users.noreply.quidgest.pt>
2025-10-06 16:34:58 +0100

    Build version: 5 | Genio version: 374.11 | Database version: 2503 | Platform: VUE

--- GEN_QUIDGEST/MYAPP/ClientApp/apps/geniovue/src/systemInfo.ts
+++ GEN_QUIDGEST/MYAPP/ClientApp/apps/geniovue/src/systemInfo.ts
@@ -7,12 +7,12 @@ export const systemInfo = {
 	applicationName: 'My application',
 
 	genio: {
-		buildVersion: 4,
+		buildVersion: 5,
 		dbIdxVersion: 3,
 		dbVersion: '2503',
 		genioVersion: '374,11',
 		trackChangesVersion: '0',
-		assemblyVersion: '374,11.2503.0.4',
+		assemblyVersion: '374,11.2503.0.5',
 		generationDate: {
 			year: 2025,
 			month: 10,
